b98f6fafd27538de121501b53f07e1caa9d5b9e3
Eric Yang <eric@MacBook-Pro-2.attlocal.net>
2019-04-30 22:14:56 -0700

    added pictures to fashion and shop pages

--- resources/views/menu/fashion.blade.php
+++ resources/views/menu/fashion.blade.php
@@ -1,12 +1,71 @@
 @extends('layout')
 @section('title', 'Fashion')
 @section('main')
-<div class="row">
- <div class="col">
-   <div class="d-flex justify-content-center">
-     <h1>Fashion Page</h1>
-   </div>
- </div>
-</div>
-
+<div class="container-fluid">
+		<div id="border-id">
+			<div class="row" id="row-1">
+			</div>
+			<div class="row" id="womens-id">
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/0a7586eb-7a2f-40fd-bb5f-7fc5087783cd.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>SAINT LAURENT Lou Matelassé Leather Belt Bag</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/a786e577-8f61-4bb7-ac9d-b30f126c59db.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>MCM Essentials Visetos Original Small Crossbody Bag</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/8b58dbe2-5d3f-4d27-b6b0-c1bc0dab88ee.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>GIVENCHY Cross 3 Leather & Suede Crossbody Bag'</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/279a58d3-2b48-4e56-a13e-266285c02b3c.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>DANSE LENTE Phoebe Leather Crossbody Bag</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/84a95abc-7d96-422e-9d96-37bdc9c8bd1f.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>SAINT LAURENT Medium Vicky Leather Crossbody Bag</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/7f9f1df5-70e0-4806-b189-c10fdce2b447.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>YUZEFI Asher Convertible Crossbody Bag</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/f3a7b224-a576-4783-9536-400a04679203.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>MARNI Small Trunk Colorblock Leather Shoulder Bag</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/bb044eae-3c64-4155-820c-43f2dcbbe3c4.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>ALEXANDER WANG Micro Marti Nylon Crossbody Bag</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/57633568-7259-4738-98cd-725198584fde.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>JIMMY CHOO Celeste Glitter Tulle Frame Clutch</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/66b2f567-d875-49ee-a210-14aede1a52dc.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>PRADA Vitello Daino Double Compartment Leather Crossbody Bag</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/b0c980b7-68e0-46cf-965e-09f3a2f7e61d.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60"
+					class="img-thumbnail">
+					<p>BALMAIN Renaissance Metallic Leather Crossbody Bag</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://n.nordstrommedia.com/id/4e0a0ea7-fa20-40bc-906f-1ba17d1e8322.jpeg?crop=pad&pad_color=FFF&format=jpeg&trim=color&trimcolor=FFF&w=780&h=838&dpr=2&quality=60" class="img-thumbnail">
+					<p>BALENCIAGA Extra Small Ville Calfskin Saddle Bag</p>
+				</div>
+			</div>
+		</div>
+	</div> <!-- End of Container -->
 @endsection
--- resources/views/menu/shop.blade.php
+++ resources/views/menu/shop.blade.php
@@ -1,12 +1,67 @@
 @extends('layout')
 @section('title', 'Shop')
 @section('main')
-<div class="row">
- <div class="col">
-   <div class="d-flex justify-content-center">
-     <h1>Shop Page</h1>
-   </div>
- </div>
-</div>
+<div class="container-fluid">
+		<div id="border-id">
+			<div class="row" id="row-1">
+			</div>
+			<div class="row" id="mens-id">
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/016/106/997/original/351614_01.jpg.jpeg" class="img-thumbnail">
+					<p>Air Jordan 11 Retro 'Concord' 2018</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/008/491/231/original/52015_01.jpg.jpeg" class="img-thumbnail products">
+					<p>Air Jordan 11 Retro 'Space Jam' 2016</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/008/490/227/original/235806_01.jpg.jpeg" class="img-thumbnail">
+					<p>Air Jordan 11 Retro 'Win Like '96'</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/016/596/422/original/376931_01.jpg.jpeg" class="img-thumbnail">
+					<p>Air Jordan 12 Retro 'Gym Red'</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/015/392/858/original/429772_01.jpg.jpeg" class="img-thumbnail">
+					<p>Air Jordan 11 Retro 'Platinum Tint'</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/010/222/921/original/144690_01.jpg.jpeg" class="img-thumbnail">
+					<p>Air Jordan 1 Retro High OG 'Bred Toe'</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/010/223/054/original/13607_01.jpg.jpeg"
+					class="img-thumbnail">
+					<p>Air Jordan 11 Retro 'Legend Blue' 2014</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/012/478/528/original/365514_01.jpg.jpeg"
+					class="img-thumbnail">
+					<p>Travis Scott x Air Jordan 4 Retro 'Cactus Jack'</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/012/219/525/original/335047_01.jpg.jpeg"
+					class="img-thumbnail">
+					<p>OFF-WHITE x Air Jordan 1 Retro High OG 'UNC'</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/008/487/202/original/13562_01.jpg.jpeg"
+					class="img-thumbnail">
+					<p>Air Jordan 11 Retro 'Bred' 2012</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/015/005/107/original/144205_01.jpg.jpeg"
+					class="img-thumbnail">
+					<p>Air Jordan 3 Retro OG 'Black Cement' 2018</p>
+				</div>
+				<div class="col-12 col-sm-12 col-md-4 col-lg-3">
+					<img src="https://image.goat.com/crop/1500/attachments/product_template_additional_pictures/images/015/005/083/original/377542_01.jpg.jpeg" class="img-thumbnail">
+					<p>Air Jordan 1 Retro High OG 'Pine Green'</p>
+				</div>
+			</div>
+		</div>	 <!-- end of border id -->
+	</div> <!-- End of Container -->
+
 
 @endsection
